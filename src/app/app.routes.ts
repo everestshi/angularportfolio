ce77d69b0a05f3721f7e9e6e7737200e6ee9f4b4
Everest Shi <shi.everest@gmail.com>
2024-03-19 17:15:00 -0700

    routing to detail page working

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DanceComponent } from './dance/dance.component';
 import { ResumeCodeComponent } from './resume-code/resume-code.component';
 import { CodeComponent } from './app-code/code.component';
+import { ProjectComponent } from './project/project.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full' },
@@ -12,6 +13,7 @@ export const routes: Routes = [
     { path: 'dance', component: DanceComponent, pathMatch: 'full' },
     { path: 'code', component: CodeComponent, pathMatch: 'full' },
     { path: 'developer-portfolio', component: PortfolioComponent, pathMatch: 'full'},
+    { path: 'developer-portfolio/:id', component: ProjectComponent},
     { path: 'developer-resume', component: ResumeCodeComponent, pathMatch: 'full'},
     { path: 'contact', component: ContactComponent, pathMatch: 'full'},
     { path: '**', redirectTo: '' }
